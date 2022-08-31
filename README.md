4cb3a6da74e3af67fb80e4e4acd180029c4c9027
Rishabh Srivastava <rishabh14601@gmail.com>
2022-09-01 01:57:31 +0530

    main.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ESW-Project
 > Must have java 1.8 and onem2m server
+
+>Sensors used:
+>- pm2.5, pm10 = prana blue
+>- co2 - prana green
+>- temp, humidity, voc -ES12127.
---
+++ main.py
@@ -0,0 +1,141 @@
+from oneM2M_functions import *
+from flask import Flask, render_template, redirect, url_for, jsonify
+uri_cse = "http://127.0.0.1:8080/~/in-cse/in-name"
+ae = "Indoor-Air-Pollution"
+cnt = "Data"
+
+headers = {
+    'X-M2M-Origin': 'admin:admin',
+    'Content-type': 'application/json'
+}
+
+def CreateContainers():
+    uri_ae = uri_cse+"/"+ae
+    create_ae(uri_cse, ae)
+    create_cnt(uri_ae,cnt)
+    create_cnt(uri_ae,"Data_Description")
+    create_data_cin(uri_ae+"/Data_Description","[Temperature, Humidity, CO2, VOC-Index, PM-2.5, PM-10]")
+
+# CreateContainers()
+# create_data_cin(uri_cse+"/"+ae+"/"+cnt,"[1,2,3,4,5,6]")
+
+
+app =Flask(__name__)
+@app.route("/")
+def home():
+    return redirect(url_for("Temperature"))
+@app.route("/Temperature")
+def Temperature():
+    resp = requests.get(
+        f'http://127.0.0.1:8080/~/in-cse/in-name/{ae}/{cnt}/la', headers=headers)
+    result = json.loads(resp.text)
+    data = result['m2m:cin']['con']
+    begin=0
+    temp=''
+    for dat in data:
+        if(dat==','):
+            break
+        while begin==1:
+            temp+=dat
+        if(dat=='['):
+            begin=1
+    return(jsonify(content=temp))
+    # return render_template("ui.html", content=float(temp),timestamp=result['m2m:cin']['ct'])
+
+@app.route("/Humidity")
+def Humidity():
+    resp = requests.get(
+        f'http://127.0.0.1:8080/~/in-cse/in-name/{ae}/{cnt}/la', headers=headers)
+    result = json.loads(resp.text)
+    data = result['m2m:cin']['con']
+    begin=0
+    temp=''
+    for dat in data:
+        if(dat==','):
+            begin+=1
+        if begin==1 and dat!=',':
+            temp+=dat
+        if(begin>1):
+            break
+    return(jsonify(content=temp))
+    # return render_template("ui.html", content=float(temp),timestamp=result['m2m:cin']['ct'])
+
+@app.route("/CO2")
+def CO2():
+    resp = requests.get(
+        f'http://127.0.0.1:8080/~/in-cse/in-name/{ae}/{cnt}/la', headers=headers)
+    result = json.loads(resp.text)
+    data = result['m2m:cin']['con']
+    begin=0
+    temp=''
+    for dat in data:
+        if(dat==','):
+            begin+=1
+        if begin==2 and dat!=',':
+            temp+=dat
+        if(begin>2):
+            break
+    return(jsonify(content=temp))
+    # return render_template("ui.html", content=float(temp),timestamp=result['m2m:cin']['ct'])
+
+@app.route("/VOC")
+def VOC():
+    resp = requests.get(
+        f'http://127.0.0.1:8080/~/in-cse/in-name/{ae}/{cnt}/la', headers=headers)
+    result = json.loads(resp.text)
+    data = result['m2m:cin']['con']
+    begin=0
+    temp=''
+    for dat in data:
+        if(dat==','):
+            begin+=1
+        if begin==3 and dat!=',':
+            temp+=dat
+        if(begin>3):
+            break
+    return(jsonify(content=temp))
+    # return render_template("ui.html", content=float(temp),timestamp=result['m2m:cin']['ct'])
+
+@app.route("/PM_2")
+def PM2():
+    resp = requests.get(
+        f'http://127.0.0.1:8080/~/in-cse/in-name/{ae}/{cnt}/la', headers=headers)
+    result = json.loads(resp.text)
+    data = result['m2m:cin']['con']
+    begin=0
+    temp=''
+    for dat in data:
+        if(dat==','):
+            begin+=1
+        if begin==4 and dat!=',':
+            temp+=dat
+        if(begin>4):
+            break
+    return(jsonify(content=temp))
+    # return render_template("ui.html", content=float(temp),timestamp=result['m2m:cin']['ct'])
+
+@app.route("/PM_10")
+def PM10():
+    resp = requests.get(
+        f'http://127.0.0.1:8080/~/in-cse/in-name/{ae}/{cnt}/la', headers=headers)
+    result = json.loads(resp.text)
+    data = result['m2m:cin']['con']
+    begin=0
+    temp=''
+    for dat in data:
+        if(dat==']'):
+            break
+        if(dat==','):
+            begin+=1
+        if begin==5 and dat!=',':
+            temp+=dat
+        
+    return(jsonify(content=temp))  
+    # return render_template("ui.html", content=float(temp),timestamp=result['m2m:cin']['ct'])
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
